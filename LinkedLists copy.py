883e75dee8b955d27341da7ce41e08927b9a7958
Chris Hunter <chriswhunter89@gmail.com>
2022-05-20 15:51:01 +0100

    linked lists completed

--- LinkedLists copy.py
+++ LinkedLists copy.py
@@ -1,13 +1,10 @@
-from operator import truediv
-
-
 class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
 
 class LinkedList:
-    def __init__(self, value):
+    def __init__(self,value):
         new_node = Node(value)
         self.head = new_node
         self.tail = new_node
@@ -35,8 +32,8 @@ class LinkedList:
             return None
         temp = self.head
         pre = self.head
-        while(temp.next):
-            pre=temp
+        while temp.next:
+            pre = temp
             temp = temp.next
         self.tail = pre
         self.tail.next = None
@@ -44,7 +41,7 @@ class LinkedList:
         if self.length == 0:
             self.head = None
             self.tail = None
-        return temp.value
+        return temp
 
     def prepend(self, value):
         new_node = Node(value)
@@ -54,19 +51,19 @@ class LinkedList:
         else:
             new_node.next = self.head
             self.head = new_node
-            self.length += 1
-            return True
+        self.length += 1
+        return True
 
     def pop_first(self):
         if self.length == 0:
             return None
         temp = self.head
         self.head = self.head.next
-        temp.next == None
+        temp.next = None
         self.length -= 1
         if self.length == 0:
             self.tail = None
-        return temp
+        return  temp
 
     def get(self, index):
         if index < 0 or index >= self.length:
@@ -74,10 +71,10 @@ class LinkedList:
         temp = self.head
         for _ in range(index):
             temp = temp.next
-        return temp
+            return temp
 
     def set_value(self, index, value):
-        temp = self.get(index)
+        temp = self.get(value)
         if temp:
             temp.value = value
             return True
@@ -97,18 +94,18 @@ class LinkedList:
         self.length += 1
         return True
 
-    def remove(self, index):
+    def remove(self, index, value):
         if index < 0 or index >= self.length:
             return None
         if index == 0:
             return self.pop_first()
         if index == self.length - 1:
             return self.pop()
-        pre = self.get(index - 1)
+        pre = self.get(index-1)
         temp = pre.next
         pre.next = temp.next
         temp.next = None
-        self.length += 1
+        self.length -= 1
         return temp
 
     def reverse(self):
@@ -122,6 +119,9 @@ class LinkedList:
             temp.next = before
             before = temp
             temp = after
+
+
+
 
 
 
